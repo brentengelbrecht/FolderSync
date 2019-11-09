f12a426d928b032bab1f4f9006ce1a9f8a866154
Brent Engelbrecht <brent@zooloo.co.za>
2019-11-09 13:16:55 +0200

    Add logging mechanism for Event Viewer

---
+++ FolderSync/ILogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolderSync
+{
+    public interface ILogger
+    {
+        void LogToInfo(String message);
+        void LogToWarning(String message);
+        void LogToError(String message);
+    }
+}
---
+++ FolderSync/WindowsEventLog.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolderSync
+{
+    class WindowsEventLog : ILogger
+    {
+        private String applicationName;
+
+        public WindowsEventLog(String applicationName)
+        {
+            this.applicationName = applicationName;
+        }
+
+        public void LogToError(string message)
+        {
+            using (EventLog eventLog = new EventLog(applicationName))
+            {
+                eventLog.Source = applicationName;
+                eventLog.WriteEntry(message, EventLogEntryType.Error);
+            }
+        }
+
+        public void LogToInfo(string message)
+        {
+            using (EventLog eventLog = new EventLog(applicationName))
+            {
+                eventLog.Source = applicationName;
+                eventLog.WriteEntry(message, EventLogEntryType.Information);
+            }
+        }
+
+        public void LogToWarning(string message)
+        {
+            using (EventLog eventLog = new EventLog(applicationName))
+            {
+                eventLog.Source = applicationName;
+                eventLog.WriteEntry(message, EventLogEntryType.Warning);
+            }
+        }
+    }
+}
